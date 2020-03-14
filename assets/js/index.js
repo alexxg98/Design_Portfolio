a3bf6378f487d6b10b1c80026fe389ac81daad34
Alexandria <lexieguo@gmail.com>
2020-03-14 19:01:59 -0400

    Hover animation, and open modal onclick for each image

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,3 +40,32 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("poem");
+var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks outside of the image, close the modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
