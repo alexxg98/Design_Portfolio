c547ebab63962cb2bad8c210e3b17df0464de0e2
Alexandria <lexieguo@gmail.com>
2020-03-14 19:12:02 -0400

    Clean up js variables

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,23 +49,23 @@ var modal = document.getElementById("myModal");
 var img = document.getElementById("poem");
 var modalImg = document.getElementById("img01");
 // var captionText = document.getElementById("caption");
-// img.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
 
 function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("myModal").style.display = "block";
+  modalImg.src = element.src;
+  modal.style.display = "block";
+  // captionText.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
 // When the user clicks outside of the image, close the modal
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+
 }
