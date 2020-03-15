954f3a9495a4b6c66872900fa8fcc9d52e01a1f7
Alexandria <lexieguo@gmail.com>
2020-03-14 20:30:53 -0400

    Add carousel to Famous Views project modal. Thumbnail cover img, opens to rest of project.

--- assets/js/index.js
+++ assets/js/index.js
@@ -67,5 +67,46 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+  if (event.target == document.getElementById("myModal_carousel")) {
+    document.getElementById("myModal_carousel").style.display = "none";
+    document.getElementById("myModal_carousel").style.display = "none";
+  }
+}
+
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal_carousel").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal_carousel").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("imgSlides");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
 
 }
