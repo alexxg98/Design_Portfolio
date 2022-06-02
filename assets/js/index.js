e7e1c9e3dff40969451b8736addaa75862b983af
Alexandria <alexandria.y.guo@gmail.com>
2022-06-02 12:05:38 -0400

    Update to Bootstrap5. Fix Filter button (readd classes)

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,8 +37,8 @@ function removeClass(element, name) {
 }
 
 // Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var filterContainer = document.getElementById("filterMenu");
+var btns = filterContainer.getElementsByClassName("filterBtn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
     var current = document.getElementsByClassName("active");
