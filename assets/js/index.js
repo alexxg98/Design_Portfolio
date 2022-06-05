dc25578d2d41cfd73a157362b8b8410d2af77b1b
Alexandria <alexandria.y.guo@gmail.com>
2022-06-04 21:38:09 -0400

    Replace add/rmClass func with builtin. Add scrollTo top when filter changes.

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,30 +10,10 @@ function filterSelection(c) {
   x = document.getElementsByClassName("filterDiv");
   if (c == "all") c = "";
   for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    x[i].classList.remove("show");
+    if (x[i].className.indexOf(c) > -1) x[i].classList.add("show");
   }
-}
-
-function addClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-  }
-}
-
-function removeClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
+  window.scrollTo(0, 0);
 }
 
 // Add active class to the current button (highlight it)
