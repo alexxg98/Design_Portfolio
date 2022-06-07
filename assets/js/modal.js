f506d15bd48976b02ecfc2cd2410e72c3935d98b
Alexandria <alexandria.y.guo@gmail.com>
2022-06-06 20:58:37 -0400

    Add projs: creativeBrain, urbanFarmIcon, shapeToSym gif. Add new filter.

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -16,6 +16,9 @@ var slideIndex = 1;
 var proj_selection = "";
 var famViews_carousel = document.getElementById("famViews_carousel");
 var yousafzai_carousel = document.getElementById("yousafzaiPortrait_carousel");
+var conPoem_carousel = document.getElementById("conPoem_carousel");
+var creativeBrain_carousel = document.getElementById("creativeBrain_carousel");
+var urbanFarmIcons_carousel = document.getElementById("urbanFarmIcons_carousel");
 
 function openCarousel(selection) {
   // currentSlide(1);
@@ -23,16 +26,27 @@ function openCarousel(selection) {
     case 'famViews':
       famViews_carousel.style.display = "block";
       proj_selection = "famViews_img";
-      showSlides(1);
       break;
     case 'yousafzaiPortrait':
       yousafzai_carousel.style.display = "block";
       proj_selection = "yousafzaiPortrait_img";
-      showSlides(1);
+      break;
+    case 'conPoem':
+      conPoem_carousel.style.display = "block";
+      proj_selection = "conPoem_img";
+      break;
+    case 'creativeBrain':
+      creativeBrain_carousel.style.display = "block";
+      proj_selection = 'creativeBrain_img';
+      break;
+    case 'urbanFarms':
+      urbanFarmIcons_carousel.style.display = "block";
+      proj_selection = 'urbanFarmIcons_img';
       break;
     default:
       break;
   }
+  showSlides(1);
 }
 
 // Next/previous controls
@@ -59,6 +73,9 @@ function closeModal() {
   singleImgModal.style.display = "none";
   famViews_carousel.style.display = "none";
   yousafzai_carousel.style.display = "none";
+  conPoem_carousel.style.display = "none";
+  creativeBrain_carousel.style.display = "none";
+  urbanFarmIcons_carousel.style.display = "none";
 }
 
 // CLose the Modal when user clicks outside image
@@ -70,6 +87,15 @@ window.onclick = function(event) {
     closeModal()
   }
   if (event.target == yousafzai_carousel) {
+    closeModal()
+  }
+  if (event.target == conPoem_carousel) {
+    closeModal()
+  }
+  if (event.target == creativeBrain_carousel) {
+    closeModal()
+  }
+  if (event.target == urbanFarmIcons_carousel) {
     closeModal()
   }
 }
